de07a943066dc9dd9b3dfb675cd0b2263755baad
muschellij2 <muschellij2@gmail.com>
2016-08-18 17:31:11 -0400

    tring to fix makefile

--- src/camino/matlab/twotenfit.m
+++
@@ -1,9 +0,0 @@
-function output = twotenfit(options, inputdata)
-
-
-options.inversion = 31;
-if isfield(options.cyl, 'cylsym')
-    options.inversion = 11;
-end
-
-output = modelfit(options, inputdata);
--- src/camino/tractography/OutsideLUTRangeException.java
+++
@@ -1,9 +0,0 @@
-package tractography;
-
-public class OutsideLUTRangeException extends Exception {
-	
-	public OutsideLUTRangeException(String message) {
-	    super(message);
-	}
-
-    }
