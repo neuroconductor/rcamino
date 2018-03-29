1f63bdbe5abb0f0bdd63e3083f8b5c688ce9911b
muschellij2 <muschellij2@gmail.com>
2018-03-29 15:47:37 -0400

    Added the copier.

--- src/copier.R
+++ src/copier.R
@@ -1,9 +1,9 @@
 dir.create('../inst/camino', recursive = TRUE)
-x = list.dirs(path = 'camino'); 
-sapply(file.path("..", "inst", x), 
-	dir.create, recursive = TRUE, 
+x = list.dirs(path = 'camino');
+sapply(file.path("..", "inst", x),
+	dir.create, recursive = TRUE,
 	showWarnings = FALSE);
-files = list.files(path = 'camino', 
-	recursive = TRUE); 
-to = file.path('../inst/camino', files); 
-file.rename(file.path('camino', x), to);
+files = list.files(path = 'camino',
+	recursive = TRUE);
+to = file.path('../inst/camino', files);
+file.rename(file.path('camino', files), to);
