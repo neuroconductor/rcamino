458ec47e588d3af893352d1eceb26307da04102d
muschellij2 <muschellij2@gmail.com>
2018-03-29 15:44:18 -0400

    may work on windows now

---
+++ src/copier.R
@@ -0,0 +1,9 @@
+dir.create('../inst/camino', recursive = TRUE)
+x = list.dirs(path = 'camino'); 
+sapply(file.path("..", "inst", x), 
+	dir.create, recursive = TRUE, 
+	showWarnings = FALSE);
+files = list.files(path = 'camino', 
+	recursive = TRUE); 
+to = file.path('../inst/camino', files); 
+file.rename(file.path('camino', x), to);
